90d656c094425b7adebed9ce01ef2c13ee945554
Dan <bmdmartinez@gmail.com>
2021-10-09 18:29:50 -0400

    hover functionality works in a basic form, working on getting the style to work as intended and making the hover seamlessly work instead of occassionally

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -10,13 +10,16 @@ function Projects() {
     const offHover = () => {
         setHover(false);
     }
-    const hoverText = () => {
-        <div className="hoverText">
-            <p>
-                Test
-            </p>
-        </div>
-    }
+    const hoverText =  <p className="hoverText" style={{ 
+        backgroundImage: `url(${background1})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100%',
+        width: '500px',
+        height: '600px',
+        opacity: '50%'
+        }} > <a href="https://danm1996.github.io/spontaneous-selection/" target="_blank" class="project-item-4">
+        <span>Spontaneous Selection</span>
+    </a> </p>;
 
     console.log(hoverText)
 
@@ -29,16 +32,14 @@ function Projects() {
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: '100%',
                 width: '500px',
-                height: '600px',
+                height: '250px',
                 }}
                 onMouseEnter={onHover}
                 onMouseLeave={offHover}
                 >
                     {hover ? hoverText : ''}
             </section>
-            <a href="https://danm1996.github.io/spontaneous-selection/" target="_blank" class="project-item-4">
-                <span>Spontaneous Selection<br />HTML/CSS/JavaScript/APIs</span>
-            </a>
+            
             </div>
         </>
     )
