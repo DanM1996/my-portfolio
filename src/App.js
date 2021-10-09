ff961a2a39fac6364d129dd352539821aa955ba9
Dan <bmdmartinez@gmail.com>
2021-10-09 17:57:22 -0400

    started implenting style, working on projects section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
 import Pages from './components/Pages';
 // import './App.css';
 
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import background1 from '../../assets/images/Spontaneous-Selection.PNG'
 
 function Projects() {
+
+    const [hover, setHover] = useState(false)
+    const onHover = () => {
+        setHover(true);
+    }
+    const offHover = () => {
+        setHover(false);
+    }
+    const hoverText = () => {
+        <div className="hoverText">
+            <p>
+                Test
+            </p>
+        </div>
+    }
+
+    console.log(hoverText)
+
     return (
-        <p>Projects go here!</p>
+        <>
+            <p>Projects go here!</p>
+            <div className='project-containers'>
+            <section style={{ 
+                backgroundImage: `url(${background1})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: '100%',
+                width: '500px',
+                height: '600px',
+                }}
+                onMouseEnter={onHover}
+                onMouseLeave={offHover}
+                >
+                    {hover ? hoverText : ''}
+            </section>
+            <a href="https://danm1996.github.io/spontaneous-selection/" target="_blank" class="project-item-4">
+                <span>Spontaneous Selection<br />HTML/CSS/JavaScript/APIs</span>
+            </a>
+            </div>
+        </>
     )
 }
 
