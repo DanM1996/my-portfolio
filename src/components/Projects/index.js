649b41d78526a3a9fd03c61f29aa093888ad2d2d
Dan <bmdmartinez@gmail.com>
2021-11-01 12:41:03 -0400

    updated resume, projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import image1 from "../../assets/images/Blue42.PNG";
-import image2 from "../../assets/images/Spontaneous-Selection.PNG";
-import image3 from "../../assets/images/Budget.PNG";
-import image4 from "../../assets/images/Password.PNG";
+import image1 from "../../assets/images/Password.PNG";
+import image2 from "../../assets/images/Blue42.PNG";
+import image3 from "../../assets/images/Spontaneous-Selection.PNG";
+import image4 from "../../assets/images/Budget.PNG";
 import image5 from "../../assets/images/Schedule.PNG";
 import image6 from "../../assets/images/Notes.PNG";
 
